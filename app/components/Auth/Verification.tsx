095b91da7ca5e31fa4350ac02bee368c866af242
Mayank Nandwani <mayankn@regenesys.net>
2025-09-29 10:18:52 +0530

    update to check the spam folder

--- app/components/Auth/Verification.tsx
+++ app/components/Auth/Verification.tsx
@@ -77,7 +77,7 @@ const Verification: FC<Props> = ({ setRoute }) => {
 
   return (
     <div>
-      <h1 className={`${styles.title}`}>Verify Your Account - Enter OTP from your mail</h1>
+      <h1 className={`${styles.title}`}>Verify Your Account - Enter OTP from your mail (check the spam folder as well)</h1>
       <br />
       <div className="w-full flex items-center justify-center mt-2">
         <div className="w-[80px] h-[80px] rounded-full bg-[#497DF2] flex items-center justify-center">
